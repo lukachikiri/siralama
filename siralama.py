c2c9e1bce733e47541e5b6b8e53e884482660afc
lukachikiri <fatihkaganusta53@gmail.com>
2022-05-30 13:02:37 +0300

    Add files via upload

---
+++ siralama.py
@@ -0,0 +1,8 @@
+sayi1, sayi2, sayi3=8,2,5
+if (sayi1 > sayi2):
+    sayi1,sayi2=sayi2, sayi1
+if (sayi1 > sayi3):
+    sayi1, sayi3=sayi3,sayi1
+if (sayi2>sayi3):
+    sayi2,sayi3=sayi3=sayi2
+print(sayi1,sayi2,sayi3)
